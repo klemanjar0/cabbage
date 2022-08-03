bfb83c306c28801e4a8f1123e4aed283508b290f
Klym Kravchenko <klymkravchenko@ecisys.com>
2022-08-03 10:38:49 +0300

    init

--- App.tsx
+++ App.tsx
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Ionicons from '@expo/vector-icons/Ionicons';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
+      <Ionicons name="md-checkmark-circle" size={32} color="green" />
       <StatusBar style="auto" />
     </View>
   );
